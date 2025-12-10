a6ad0464f3ba92c32f2f0c27f261b6574b1390f6
Edward Denis <edorward.21@gmail.com>
2025-12-10 11:31:27 +0300

    Added .env and .gitignore files

--- To_Do_Project/To_Do_Project/settings.py
+++ To_Do_Project/To_Do_Project/settings.py
@@ -12,21 +12,24 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j1g+#4p6!iw047g2#gix%s+xtogp-tkie_wfbg14q9emln-edd'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWWED_HOSTS', default=[])
 
 
 # Application definition
