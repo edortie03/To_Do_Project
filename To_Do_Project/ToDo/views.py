f2f3bdbc0613ef28ae59ba28da5c0e1835893261
Edward Denis <edorward.21@gmail.com>
2025-12-11 12:04:10 +0300

    Added views logic

--- To_Do_Project/ToDo/views.py
+++ To_Do_Project/ToDo/views.py
@@ -4,4 +4,16 @@ from django.http import HttpResponse
 # Create your views here.
 
 def base(request):
-    return render('This is a base page')
+    return HttpResponse('This is a base page')
+
+def login(request):
+    return HttpResponse('This is a login page')
+
+def register(request):
+    return HttpResponse('This is a register page') 
+
+def logout(request):
+    return HttpResponse('Successfully logged out')
+
+def dashboard(request):
+    return HttpResponse('This is the dashboard page')
