3915a5f2e5362dd88d7b313e7b13645f6f5cff67
Doreen Enock Kamando <doreenenock2005@gmail.com>
2025-12-11 11:58:30 -0800

    make some changes on model

--- To_Do_Project/ToDo/models.py
+++ To_Do_Project/ToDo/models.py
@@ -2,8 +2,3 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here
-class Task(models.model):
-    user = 
-    title = 
-    description = 
-    complete =
