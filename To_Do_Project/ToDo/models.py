f0c59623f8959f7388254b93dc0196ede03a04b1
Edward Denis <edorward.21@gmail.com>
2025-12-12 00:49:59 +0300

    Created a Task Table

--- To_Do_Project/ToDo/models.py
+++ To_Do_Project/ToDo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+    createdd_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
